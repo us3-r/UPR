540e37472f43a5d5b3668ff65ed6b70d8aa3477e
us3-r <z4nhr1@gmail.com>
2022-12-06 18:33:03 +0100

    \^o^/

--- UPR/classes.py
+++ UPR/classes.py
@@ -6,7 +6,7 @@ class Pass:
     pass
 class Prompt:
     File_req:str = f"{Prefix.p}Pleas enter a path to file: "
-    Rows:int     = f"{Prefix.p}Enter the number of rows in your excel file: "
+    Rows:int     = f"{Prefix.p}Enter the number of rows (minus the row with the titles) in your excel file: "
     Output_file:str = f"{Prefix.p}Enter a name/path of/to your output file: "
 class Dict:
     def __init__(self) -> None:
