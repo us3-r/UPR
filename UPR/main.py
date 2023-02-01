70ad843fab9e05dd9807ed81ea6e9faad8f1f426
us3-r <z4nhr1@gmail.com>
2023-02-01 22:08:36 +0100

    fixed a few issues

--- UPR/main.py
+++ UPR/main.py
@@ -30,7 +30,10 @@ ui.ui_main() # opens a ui to select files
 #     file_for_update = input("Which file would you like to update? ")
 #     update_req.append(file_for_update)
 
-#     # exit()
+    # exit()
+
+
+# clear config.json and write new data
 
 
 
@@ -39,6 +42,7 @@ with open("config.json","r") as file:
 input__ = data["input_file"]
 output__ = data["output_file"]
 rows__ = data["rows"]
+
 
 data = p.read_excel(input__)
 
@@ -49,6 +53,9 @@ title = t.TitleFormat.missingTitle(title) # gets all titles added to one list
 current_block_type = ""
 previous_block_type = ""
 same = 0
+
+with open(output__,"a") as out_file:
+    out_file.write(f"{t.TitleFormat.return_top_seq(output__)}\n\n")
 
 for r in range(int(rows__)):
     d = data.loc[r].values # returns a list of all the values from that row
@@ -90,19 +97,39 @@ for r in range(int(rows__)):
     with open(output__,"a") as out_file:
         print(f"\n[?] WRITING UNDER BLOCK TYPE: {current_block_type} ")
         if  previous_block_type == current_block_type and same < 1:
-            out_file.write(str(list(default_data.keys())))
-            out_file.write(f"\n{t.TitleFormat.low_title}")
+            out_file.write(f"\n\n{str(list(default_data.keys()))}")
+
+            type_ = t.SelectJson.select_type(str(d[0]))
+            lower_title = t.TitleFormat.low_title_AI if type_ == "AI" else t.TitleFormat.low_title_AO if type_ == "AO" else t.TitleFormat.low_title_DI if type_ == "DI" else t.TitleFormat.low_title_DO if type_ == "DO" else None
+
+            out_file.write(f"\n{lower_title if lower_title != None else '[!] ERROR UNSUPPORTED TYPE'}\n")
             data_row = ';'.join([f'"{str(value)}"' if value != ';' else '""' for value in default_data.values()])
-            out_file.write(f"\n{data_row}\n")
+            out_file.write(f"\n{data_row}")
+
             same += 1
         elif previous_block_type == current_block_type:
             data_row = ';'.join([f'"{str(value)}"' if value != ';' else '""' for value in default_data.values()])
-            out_file.write(f"\n{data_row}\n")
+            out_file.write(f"\n{data_row}")
             same += 1
         else:
             # Write a blank line after the data row
             out_file.write("\n")
+
     print(f"[?] SUCCESSFULLY WRITTEN ROW {r} TO {output__}")
+
+with open(output__,"a") as out_file:
+    out_file.write(f"\n\n{t.TitleFormat.end_block}")
+
 print(f"\n+++++++++++++++++++++++++++++++++++++++++++++++")
 print(f"+++++++++++++++++ DONE ++++++++++++++++++++++++")
 
+
+# reset config.json
+dic = {
+    "input_file": "",
+    "output_file": "",
+    "rows":""
+}
+js_obj = json.dumps(dic, indent=4)
+with open("config.json", "w") as f:
+    f.write(js_obj)
